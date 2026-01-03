3a03e6c70c7a289dd03d49403c4b2345dddd3c0f
Kojo.Mante-Dankwa <kojonhyira17@gmail.com>
2026-01-03 23:08:04 +0000

    Update

--- src/main/java/Model/Transaction.java
+++ src/main/java/Model/Transaction.java
@@ -30,4 +30,5 @@ public class Transaction {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return String.format("%-12s | GHS %,10.2f | %s | Balance: GHS %,10.2f", transactionType, amount, dateTime.format(formatter), balanceAfter);
     }
+
 }
